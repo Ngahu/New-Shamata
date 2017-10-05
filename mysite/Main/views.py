0f6556f4bfb4b354bc91b01595505b115b2a9da9
Ngahu <jamaalaraheem@gmail.com>
2017-10-05 15:31:58 +0300

    creating of the urls file and also wiring up of the url with the view function

--- mysite/Main/views.py
+++ mysite/Main/views.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from .models import Property,TeamMember
 # Create your views here.
+
+
+def home(request):
+    context ={
+        "title":"title"
+    }
+    template = 'index.html'
+    return render(request,template,context)
